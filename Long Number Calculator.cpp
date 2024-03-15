779a6ce7e122ab77558a50510240737143c06425
c0rdyy <aproovloashv@yandex.ru>
2024-03-15 18:46:59 +0300

    Добавьте файлы проекта.

---
+++ Long Number Calculator.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int t;
+
+	printf("Добро пожаловать в калькулятор длинных чисел!\n");
+    
+	switch (t)
+	{
+	case 1:
+		break;
+	case 2:
+		break;
+	default:
+		break;
+	}
+}
