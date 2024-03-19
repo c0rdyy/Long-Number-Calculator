fdee26fd21a1a901b88e0fb68ec5a9bdb64b3a29
c0rdyy <aproovloashv@yandex.ru>
2024-03-20 00:33:05 +0300

    Реализована поддержка отрицательных чисел при вычитании

--- Functions.cpp
+++ Functions.cpp
@@ -154,80 +154,109 @@ void addNumbers(char* num1, char* num2, char* result)
     // Вывод результата без лишних нулей
     if (first_digit_pos == MAX - 1) // Все цифры нули
     {
-        printf("Сумма: 0\n\n"); // Выводим один ноль
+        printf("Результат: 0\n\n"); // Выводим один ноль
     }
     else
     {
         if (is_num1_negative && is_num2_negative)
         {
-            printf("Сумма: -%s\n\n", result + first_digit_pos); // Вывод результата без лишних нулей
+            printf("Результат: -%s\n\n", result + first_digit_pos); // Вывод результата без лишних нулей
         }
 
         else if (is_num1_negative == 0 && is_num2_negative == 0)
         {
-            printf("Сумма: %s\n\n", result + first_digit_pos); // Вывод результата без лишних нулей
+            printf("Результат: %s\n\n", result + first_digit_pos); // Вывод результата без лишних нулей
         }
 
     }
 }
 
 // Вычитание
-void subtract(char num1[], char num2[], char result[]) 
+void subtract(char num1[], char num2[], char result[])
 {
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    int res[MAX] = { 0 };
+    int is_num1_negative = (num1[0] == '-');
+    int is_num2_negative = (num2[0] == '-');
 
-    int negative = 0;
-    if (len1 < len2 || (len1 == len2 && strcmp(num1, num2) < 0)) 
+    if (is_num1_negative && !is_num2_negative)
     {
-        subtract(num2, num1, result);
-        negative = 1;
-    }
-    else 
-    {
-        for (int i = 0; i < len1; i++) 
-        {
-            res[i] = num1[len1 - i - 1] - '0';
-        }
-
-        for (int i = 0; i < len2; i++) 
-        {
-            res[i] -= num2[len2 - i - 1] - '0';
-        }
-
-        for (int i = 0; i < MAX - 1; i++) 
-        {
-            if (res[i] < 0) 
-            {
-                res[i] += 10;
-                res[i + 1]--;
-            }
-        }
-
-        int index = MAX - 1;
-        while (index >= 0 && res[index] == 0) 
-        {
-            index--;
-        }
-
-        if (index == -1) 
-        {
-            strcpy(result, "0");
-        }
-        else {
-            for (int i = index; i >= 0; i--)
-            {
-                result[index - i] = res[i] + '0';
-            }
-            result[index + 1] = '\0';
-        }
+        addNumbers(num1, num2, result);
+        printf("Результат: %s\n\n", result);
+        return;
     }
 
-    if (negative) 
+    else if (is_num1_negative && is_num2_negative)
     {
-        memmove(result + 1, result, strlen(result) + 1);
-        result[0] = '-';
+        addNumbers(num1, num2 + 1, result);
+    }
+
+    else if (!is_num1_negative && is_num2_negative)
+    {
+        addNumbers(num1, num2 + 1, result);
+    }
+
+    else
+    {
+        int len1 = strlen(num1);
+        int len2 = strlen(num2);
+        int res[MAX] = { 0 };
+
+        int negative = 0;
+        if (len1 < len2 || (len1 == len2 && strcmp(num1, num2) < 0))
+        {
+            subtract(num2, num1, result);
+            negative = 1;
+            memmove(result + 1, result, strlen(result) + 1);
+            result[0] = '-';
+            printf("Результат: %s\n\n", result);
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < len1; i++)
+            {
+                res[i] = num1[len1 - i - 1] - '0';
+            }
+
+            for (int i = 0; i < len2; i++)
+            {
+                res[i] -= num2[len2 - i - 1] - '0';
+            }
+
+            for (int i = 0; i < MAX - 1; i++)
+            {
+                if (res[i] < 0)
+                {
+                    res[i] += 10;
+                    res[i + 1]--;
+                }
+            }
+
+            int index = MAX - 1;
+            while (index >= 0 && res[index] == 0)
+            {
+                index--;
+            }
+
+            if (index == -1)
+            {
+                strcpy(result, "0");
+            }
+            else {
+                for (int i = index; i >= 0; i--)
+                {
+                    result[index - i] = res[i] + '0';
+                }
+                result[index + 1] = '\0';
+            }
+
+        }
+
+        if (negative)
+        {
+            memmove(result + 1, result, strlen(result) + 1);
+            result[0] = '-';
+        }
+        printf("Результат: %s\n\n", result);    
     }
 }
 
